4856b9b0779b7f5b17069641668f3c67a6c7cff7
Kamil <m.kowalski767@wp.pl>
2016-01-26 18:42:41 +0100

    ORW3

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;//w ww. jav a  2s .c  om
+using System.Net;
+using System.Net.Sockets;
+
+public class Example
+{
+    public static void Main()
+    {
+        string data = "data";
+        WebClient client = new WebClient();
+
+        client.Encoding = System.Text.Encoding.UTF8;
+
+        string reply = client.UploadString("http://www.onet.pl", data);
+
+        Console.WriteLine(reply);
+
+    }
+}
